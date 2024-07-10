6fc801baf4eea6049ab798e746cbaa992e37fa5f
Hymiko5 <86249135+Hymiko5@users.noreply.github.com>
2024-07-10 16:16:21 +0700

    update

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #--------- Flask settings
 SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
-SERVER_PORT = 5001
+SERVER_PORT = 5000
 FLASK_DEBUG = True # Do not use debug mode in prod
 
 # Flask-Restplus settings
